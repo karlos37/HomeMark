447d62af438481595fc38be06b740d1dcc57b58b
SBodapati11 <shanmukhabodapati@gmail.com>
2024-03-27 01:57:26 -0500

    initial demo

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Main : MonoBehaviour
+{
+    [SerializeField] Canvas mainMenu;
+	[SerializeField] Canvas joinMenu;
+
+    [SerializeField] GameObject theater;
+
+	[SerializeField] private XRCardboardInputModule vrInputModule;
+	[SerializeField] private StandaloneInputModuleCopy standardInputModuleCopy;
+
+	private string[] mainMenuNames;
+	private string[] joinMenuNames;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		mainMenu.enabled = true;
+		joinMenu.enabled = false;
+		theater.SetActive(false);
+
+		mainMenuNames = new string[2] { "Create Room Button", "Join Room Button" };
+		joinMenuNames = new string[2] { "Button", "Button (1)" };
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (mainMenu.enabled)
+		{
+			if (vrInputModule.enabled)
+			{
+				standardInputModuleCopy.enabled = true;
+				vrInputModule.enabled = false;
+
+				EventSystem.current.SetSelectedGameObject(null);
+				EventSystem.current.SetSelectedGameObject(mainMenu.transform.Find("IntroPanel").Find("Create Room Button").gameObject);
+			}
+
+			navigateMainMenu();
+		}
+		else if (joinMenu.enabled)
+		{
+			navigateJoinMenu();
+		}
+		
+
+	}
+
+    void navigateMainMenu()
+    {
+		if (Input.GetButtonDown("js2"))
+		{
+			if (EventSystem.current.currentSelectedGameObject == mainMenu.transform.Find("IntroPanel").Find("Join Room Button").gameObject)
+			{
+				mainMenu.enabled = false;
+				joinMenu.enabled = true;
+
+				EventSystem.current.SetSelectedGameObject(null);
+				EventSystem.current.SetSelectedGameObject(joinMenu.transform.Find("Panel").Find("Button").gameObject);
+				Debug.Log(standardInputModuleCopy.enabled);
+			}
+		}
+	}
+
+	void navigateJoinMenu()
+	{
+		if (Input.GetButtonDown("js2"))
+		{
+			if (EventSystem.current.currentSelectedGameObject == joinMenu.transform.Find("Panel").Find("Button").gameObject)
+			{
+				mainMenu.enabled = false;
+				joinMenu.enabled = false;
+				theater.SetActive(true);
+
+				standardInputModuleCopy.enabled = false;
+				vrInputModule.enabled = true;
+			}
+		}
+	}
+}
