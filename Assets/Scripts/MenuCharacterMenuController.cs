ad522c31f09ecf92af022cad56c83d7131e40e43
Soham Mukherjee <sohammukherjee2000@gmail.com>
2024-04-06 18:45:12 -0500

    base menu shows up in front of users, also disables movement when looking at menu

--- Assets/Scripts/MenuCharacterMenuController.cs
+++ Assets/Scripts/MenuCharacterMenuController.cs
@@ -4,16 +4,42 @@ using UnityEngine;
 
 public class MenuCharacterMenuController : MonoBehaviour
 {
-    public GameObject menu;
+    private GameObject menu;
+    private CharacterMovement characterMovement;
+    private float menuDistFromUser = 2.0f;
+    private bool menuActive = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        menu = transform.Find("XRCardboardRig/HeightOffset/Menu").gameObject;
+        characterMovement = GetComponentInChildren<CharacterMovement>();
+        menuActive = false;
+        menu.SetActive(menuActive);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        menu.SetActive(menuActive);
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            Debug.Log("Menu button pressed!");
+            // spawn menu in front of user
+            Vector3 userPosition = transform.position;
+            Vector3 menuPosition = userPosition + transform.forward * menuDistFromUser;
+
+            menu.transform.position = menuPosition;
+            menuActive = !menuActive;
+
+        }
+        if (menuActive)
+        {
+            characterMovement.enabled = false;
+        }
+        else
+        {
+            characterMovement.enabled = true;
+        }
     }
 }
