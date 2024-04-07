c9a79a2d483483690999c1477494394eb9e072be
Soham Mukherjee <sohammukherjee2000@gmail.com>
2024-04-07 17:25:44 -0500

    Volume control in menu works

--- Assets/Scripts/VolumeController.cs
+++ Assets/Scripts/VolumeController.cs
@@ -1,23 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class VolumeController : MonoBehaviour
 {
+    public VideoPlayer[] videoPlayers;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        videoPlayers = FindObjectsOfType<VideoPlayer>();
+        //
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        videoPlayers = FindObjectsOfType<VideoPlayer>();
     }
 
     public void ChangeVolume(float amt)
     {
-        Debug.Log("<color=yellow>Volume: </color>" + amt);
+        foreach (var videoPlayer in videoPlayers)
+        {
+            float new_vol;
+            float cur_vol = videoPlayer.GetDirectAudioVolume(0);
+            new_vol = Mathf.Clamp(cur_vol + amt, 0, 1);
+            videoPlayer.SetDirectAudioVolume(0, new_vol);
+        }
     }
 }
